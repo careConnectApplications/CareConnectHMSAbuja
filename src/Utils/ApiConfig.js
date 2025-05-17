e0dce3c542ba9755cc32d4e12ff26a42d600a77e
Adeleke Moyinoluwa Solomon <obinnaedmund121@gmail.com>
2025-05-17 14:02:40 +0100

    Update ApiConfig.js

--- src/Utils/ApiConfig.js
+++ src/Utils/ApiConfig.js
@@ -1,10 +1,10 @@
 // Test Base URL
-export const baseUrl = "http://20.164.19.147:5000/api/v1";
+// export const baseUrl = "http://20.164.19.147:5000/api/v1";
 
 export const nigeriaStateApiUrl = "https://nga-states-lga.onrender.com";
 
 export const FacilityName = "GENERAL HOSPITAL, KATSINA";
-// export const baseUrl = "https://ehealthcareconnect.com/api/v1";
+export const baseUrl = "https://ehealthcareconnect.com/api/v1";
 
 // export const FacilityName = "GENERAL HOSPITAL, FUNTUA";
 // export const baseUrl =  "https://ghfta.ehealthcareconnect.com/api/v1";
