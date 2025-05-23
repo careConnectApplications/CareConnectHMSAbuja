387695a3ada9863afc03be9f702039aceec1adff
Adeleke Moyinoluwa Solomon <obinnaedmund121@gmail.com>
2025-05-23 08:56:27 +0100

    Update ApiConfig.js

--- src/Utils/ApiConfig.js
+++ src/Utils/ApiConfig.js
@@ -1,38 +1,34 @@
+export const nigeriaStateApiUrl = "https://nga-states-lga.onrender.com";
 // Test Base URL
+// export const baseUrl = `${window.location.protocol}//${window.location.hostname}:5000/api/v1`;
 //  export const baseUrl = `http://20.164.19.147:5000/api/v1`;
 
-  export const baseUrl = `${window.location.protocol}//${window.location.hostname}:5000/api/v1`;
+export const baseUrl = `${window.location.protocol}//${window.location.hostname}/api/v1`;
 
+const baseChecker = `${window.location.hostname}`
 
-//  const baseUrl = `${window.location.protocol}//${window.location.hostname}`;
+export let FacilityName = ""
 
-export const nigeriaStateApiUrl = "https://nga-states-lga.onrender.com";
-
-// export const FacilityName = "GENERAL HOSPITAL, KATSINA";
-// export const baseUrl = "https://ehealthcareconnect.com/api/v1";
-
-// export const FacilityName = "GENERAL HOSPITAL, FUNTUA";
-// export const baseUrl =  "https://ghfta.ehealthcareconnect.com/api/v1";
-
-// export const FacilityName = "TURAI YAR'ADUA CHILDREN AND MATERNITY SPECIALIST HOSPITAL";
-// export const baseUrl =  "https://tymch.ehealthcareconnect.com/api/v1";
-
-// export const FacilityName = "GENERAL HOSPITAL, MALUMFASHI";
-// export const baseUrl =  "https://ghmlf.ehealthcareconnect.com/api/v1";
-
-// export const FacilityName = "GENERAL HOSPITAL, DUTSINMA";
-// export const baseUrl =  "https://ghdtm.ehealthcareconnect.com/api/v1";
-
-// export const FacilityName = "GENERAL HOSPITAL, KANKIA";  
-// export const baseUrl =  "https://ghknk.ehealthcareconnect.com/api/v1";
-
-export const FacilityName = "GENERAL HOSPITAL, MANI";  
-//export const baseUrl =  "https://ghman.ehealthcareconnect.com/api/v1";
-
-// export const FacilityName = "YERWA PRIMARY HEALTHCARE CENTRE, MAIDUGURI";  
-// export const baseUrl =  "https://yerwaphc.ehealthcareconnect.com/api/v1";
-
-// export const FacilityName = "MALA KACHALLA PRIMARY HEALTHCARE CENTRE, MAIDUGURI";  
-// export const baseUrl =  "https://mkachallaphc.ehealthcareconnect.com/api/v1";
+if (baseChecker === "ehealthcareconnect.com") {
+    FacilityName = "GENERAL HOSPITAL, KATSINA"
+} else if (baseChecker === "ghfta.ehealthcareconnect.com") {
+    FacilityName = "GENERAL HOSPITAL, FUNTUA"
+} else if (baseChecker === "tymch.ehealthcareconnect.com") {
+    FacilityName = "TURAI YAR'ADUA CHILDREN AND MATERNITY SPECIALIST HOSPITAL"
+} else if (baseChecker === "ghmlf.ehealthcareconnect.com") {
+    FacilityName = "GENERAL HOSPITAL, MALUMFASHI"
+} else if (baseChecker === "ghdtm.ehealthcareconnect.com") {
+    FacilityName = "GENERAL HOSPITAL, DUTSINMA"
+} else if (baseChecker === "ghknk.ehealthcareconnect.com") {
+    FacilityName = "GENERAL HOSPITAL, KANKIA"
+} else if (baseChecker === "ghman.ehealthcareconnect.com") {
+    FacilityName = "GENERAL HOSPITAL, MANI"
+} else if (baseChecker === "yerwaphc.ehealthcareconnect.com") {
+    FacilityName = "YERWA PRIMARY HEALTHCARE CENTRE, MAIDUGURI"
+} else if (baseChecker === "mkachallaphc.ehealthcareconnect.com") {
+    FacilityName = "MALA KACHALLA PRIMARY HEALTHCARE CENTRE, MAIDUGURI"
+}else if (baseChecker === "20.164.19.147") {
+    FacilityName = "TEST HOSPITAL"
+}
 
 export const token = localStorage.getItem("token");
