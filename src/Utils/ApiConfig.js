9b5d938394f40bcd59ea86515f82cd8a8270c14f
Malachi Chidiebere Egbugha <malachi.egbugha@ekedp.com>
2025-07-01 17:49:35 +0100

    addfrontend

--- src/Utils/ApiConfig.js
+++ src/Utils/ApiConfig.js
@@ -1,13 +1,13 @@
 export const nigeriaStateApiUrl = "https://nga-states-lga.onrender.com";
 // Test Base URL
 
-export const baseUrl = `${window.location.protocol}//${window.location.hostname}:5000/api/v1`;
-  // export const baseUrl = `http://20.164.19.147:5000/api/v1`;
+// export const baseUrl = `${window.location.protocol}//${window.location.hostname}:5000/api/v1`;
+//   export const baseUrl = `http://20.164.19.147:5000/api/v1`;
 
-// export const baseUrl = `${window.location.protocol}//${window.location.hostname}/api/v1`;
+export const baseUrl = `${window.location.protocol}//${window.location.hostname}/api/v1`;   
 //  export let FacilityName = "TEST HOSPITAL"
 
-const baseChecker = `${window.location.hostname}`
+const baseChecker = `${window.location.hostname}`     
 
 export let FacilityName = ""
 
@@ -32,7 +32,7 @@ if (baseChecker === "ehealthcareconnect.com") {
 }else if (baseChecker === "20.164.19.147") {
     FacilityName = "TEST HOSPITAL"
 }else {
-    FacilityName = "GENERAL HOSPITAL, FUNTUA"
+    FacilityName = "TEST HOSPITAL"
 }
 
- export const token = localStorage.getItem("token");
+export const token = localStorage.getItem("token");
