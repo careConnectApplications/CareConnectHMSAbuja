e1278e64737fe8c962f2a35849c047bd13a1d27c
Adeleke Moyinoluwa Solomon <obinnaedmund121@gmail.com>
2025-09-05 09:01:23 +0100

    Update ApiConfig.js

--- src/Utils/ApiConfig.js
+++ src/Utils/ApiConfig.js
@@ -3,9 +3,9 @@ export const nigeriaStateApiUrl = "https://nga-states-lga.onrender.com";
 
 // export const baseUrl = `${window.location.protocol}//${window.location.hostname}:5000/api/v1`;
 
-  export const baseUrl = `http://20.164.19.147:8085/api/v1` ;  
+  // export const baseUrl = `http://20.164.19.147:8085/api/v1` ;  
 
-//  export const baseUrl = `${window.location.protocol}//${window.location.hostname}/api/v1`;
+ export const baseUrl = `${window.location.protocol}//${window.location.hostname}/api/v1`;
 
 
 const baseChecker = `${window.location.hostname}`;
@@ -14,7 +14,7 @@ export let FacilityName = "";
 
 if (baseChecker === "adh.ehealthcareconnect.com") {
   FacilityName = "ASOKORO DISTRICT HOSPITAL (ADH)";
-} else if (baseChecker === "http://ngh.ehealthcareconnect.com") {
+} else if (baseChecker === "ngh.ehealthcareconnect.com") {
   FacilityName = "NYANYA GENERAL HOSPITAL";
 }else if (baseChecker === "20.164.19.147") {
   FacilityName = "TEST HOSPITAL";
